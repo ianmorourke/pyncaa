9e8da16c6bc07aa9bd35a684acbabc52fd46f8ac
ianmorourke <ianmorourke@gmail.com>
2019-09-25 07:46:01 -0400

    Add hand scoring and discards going to opponent's hand

--- ncaa.py
+++ ncaa.py
@@ -2,7 +2,7 @@ import random
 
 HAND_SIZE = 5
 DEBUG = 1
-VERBOSE = 0
+VERBOSE = 1
 
 
 class Card:
@@ -11,20 +11,20 @@ class Card:
         self.value = value
 
     def show(self):
-        switcher = {1: "Ace",
-                    2: "Two",
-                    3: "Three",
-                    4: "Four",
-                    5: "Five",
-                    6: "Six",
-                    7: "Seven",
-                    8: "Eight",
-                    9: "Nine",
-                    10: "Ten",
-                    11: "Jack",
-                    12: "Queen",
-                    13: "King",
-                    14: "Joker"
+        switcher = {
+                    1: "Two",
+                    2: "Three",
+                    3: "Four",
+                    4: "Five",
+                    5: "Six",
+                    6: "Seven",
+                    7: "Eight",
+                    8: "Nine",
+                    9: "Ten",
+                    10: "Jack",
+                    11: "Queen",
+                    12: "King",
+                    13: "Ace"
                     }
         name = switcher.get(self.value)
         print("{} of {}".format(name, self.suit))
@@ -85,8 +85,6 @@ class Player:
         for c in self.hand:
             if c.value > self.high_card:
                 self.high_card = c.value
-            else:
-                self.high_card = self.high_card
 
     def tally_hand(self):
         values = []
@@ -116,12 +114,14 @@ class Player:
 
     def set_pairs(self):
         _pairs = 0
+        _high_card = 0
         for c in self.hand_counts:
-            print(c[1])
-            if c[1] == 2:
+            if c[1] == 2 & c[0] > _high_card:
+                print("Pair of " + str(c[0] + 1))
                 _pairs = _pairs + 1
+                self.high_card = c[0]
 
-        print(_pairs)
+        # print(_pairs)
         self.pairs = _pairs
 
     def set_flush(self):
@@ -139,12 +139,22 @@ class Player:
 
     def score_hand(self):
         self.tally_hand()
-        self.set_flush()
         self.set_high_card()
-        self.set_quads()
-        self.set_straight()
-        self.set_trips()
         self.set_pairs()
+        self.set_trips()
+        self.set_straight()
+        self.set_flush()
+        # full house (1 trip, 1 pair)
+        self.set_quads()
+        # straight flush (is_straight = 1 and is_flush = 1)
+        scores = [self.pairs, self.trips, self.is_straight, self.is_flush, self.quads]
+        print(scores)
+        if scores == [0, 0, 0, 0, 0]:
+            total = self.high_card + 48
+            print("Your high card scored you " + str(total) + " points.")
+        if scores == [1, 0, 0, 0, 0]:
+            total = self.high_card + 60
+            print("Your pair scored you " + str(total) + " points.")
 
     def show_player(self):
         self.show_hand()
@@ -172,11 +182,14 @@ def main():
         player1.draw(deck)
 
     print("Your hand: ")
-    player1.show_player()
+    player1.show_hand()
 
     discards = input("Discards (separated by spaces): ").split(" ")
     # print(discards)
     # print(len(discards))
+
+    discards.reverse()
+    # print(discards)
 
     for d in discards:
         dc = player1.hand.pop(int(d) - 1)
@@ -192,10 +205,13 @@ def main():
         opponent1.draw(deck)
 
     print("Your hand: ")
-    player1.show_player()
+    player1.show_hand()
 
-    print("Your opponent's hand: ")
+    """print("Your opponent's hand: ")
     opponent1.show_player()
+"""
+    # player1.show_player()
+    player1.score_hand()
 
 
 if __name__ == "__main__":
